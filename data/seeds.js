678310eab19bbd7874afae92c711ef2df44017c6
Jose <josefbautista94@gmail.com>
2025-04-23 23:20:28 -0400

    Pushing Monday's work

---
+++ data/seeds.js
@@ -0,0 +1,8 @@
+const seedFruits = [
+    { name: 'grapefruit', color: 'pink', readyToEat: true },
+    { name: 'grape', color: 'purple', readyToEat: false },
+    { name: 'avocado', color: 'green', readyToEat: true }
+  ]
+  
+  export default seedFruits
+  
